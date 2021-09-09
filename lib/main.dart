9e800154911b21646b9a845122914e2d186e4a15
Aman Kumar <thisisamank@gmail.com>
2021-09-09 23:09:53 +0530

    repository added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:conduit_flutter/shared/repository/user_repository.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final mockRegistration = {
+      "user": {
+        "username": "test1232",
+        "email": "test212@gmail.com",
+        "password": "1234567"
+      }
+    };
+    UserRegistrationRepository('http://node-express-conduit.appspot.com/')
+        .post(input: mockRegistration);
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
   }
 }
--- lib/shared/repository/user_repository.dart
+++ lib/shared/repository/user_repository.dart
@@ -1,9 +1,30 @@
+import 'dart:convert';
+
 import 'package:conduit_flutter/shared/models/users.dart';
 import 'package:conduit_flutter/shared/repository/service.dart';
+import 'package:dio/dio.dart';
 
-// class UserRegistrationRepository extends Service<Map<String, dynamic>, User> {
-//   @override
-//   Future<User> post({required Map<String, dynamic> credentials}) {
-//     return throw UnimplementedError();
-//   }
-// }
+class UserRegistrationRepository extends Service<Map<String, dynamic>, bool> {
+  UserRegistrationRepository(this.baseUrl);
+  static const _requestHeaders = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json'
+  };
+  final String baseUrl;
+
+  @override
+  Future<bool> post({required Map<String, dynamic> input}) async {
+    final dio = Dio();
+    dio.options.baseUrl = baseUrl;
+
+    final response = await dio.post('/users', data: jsonEncode(input));
+    print(response.statusCode);
+    print(response.extra);
+    print(response.data);
+    if (response.statusCode == 201) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
